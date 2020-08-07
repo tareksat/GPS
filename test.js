8a462715670e92fbe3a9f36a803fe35207c72bd6
Tarek Morshed <tarek.morshid@gmail.com>
2020-08-07 21:37:20 +0200

    Fixing missing latitude and longitude values

---
+++ test.js
@@ -0,0 +1,7 @@
+var str = " abc;  ";
+
+str = str.trim();
+
+str = str.replace(";", "");
+
+console.log(str);
--- udp.js
+++ udp.js
@@ -1,56 +1,51 @@
-var udp = require('dgram');
-var decode = require('./msg_dec');
+var udp = require("dgram");
+var decode = require("./msg_dec");
 
 // --------------------creating a udp server --------------------
 
 // creating a udp server
-var server = udp.createSocket('udp4');
+var server = udp.createSocket("udp4");
 
 // emits when any error occurs
-server.on('error',function(error){
-  console.log('Error: ' + error);
+server.on("error", function (error) {
+  console.log("Error: " + error);
   server.close();
 });
 
 // emits on new datagram msg
-server.on('message',function(msg,info){
-  try{
+server.on("message", function (msg, info) {
+  try {
     console.log(decode(msg.toString()));
-  }
-  catch(error){
+  } catch (error) {
     console.log(error.message);
   }
-  
+
   //console.log('Data received from client : ' + msg.toString());
- // console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+  // console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
 
-//sending msg
-server.send(msg,info.port,'localhost',function(error){
-  if(error){
-    client.close();
-  }else{
-    console.log('Data sent !!!');
-  }
-
-});
-
+  //sending msg
+  server.send("ON", info.port, "localhost", function (error) {
+    if (error) {
+      client.close();
+    } else {
+    }
+  });
 });
 
 //emits when socket is ready and listening for datagram msgs
-server.on('listening',function(){
+server.on("listening", function () {
   var address = server.address();
   var port = address.port;
   var family = address.family;
   var ipaddr = address.address;
-  console.log('Server is listening at port' + port);
-  console.log('Server ip :' + ipaddr);
-  console.log('Server is IP4/IP6 : ' + family);
+  console.log("Server is listening at port" + port);
+  console.log("Server ip :" + ipaddr);
+  console.log("Server is IP4/IP6 : " + family);
 });
 
 //emits after the socket is closed using socket.close();
-server.on('close',function(){
-  console.log('Socket is closed !');
+server.on("close", function () {
+  console.log("Socket is closed !");
 });
 
 server.bind(3001);
-
